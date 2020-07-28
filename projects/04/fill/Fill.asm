055ad16339a3cd42667f6bfce3fe72cd60e65f12
Anil Dhurjaty <adhurjaty@gmail.com>
2020-07-28 10:39:31 -0700

    completes chapter 4 project

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,74 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+@24576
+D=M     // get keyboard pressed button
+@140
+D=D-A
+@END
+D;JEQ   // stop if esc is pressed
+
+@24576
+D=M
+@BLACK
+D;JGT
+@LOOP
+0;JMP
+
+(BLACK)
+@16384
+D=A
+@i
+M=D
+(BLOOP)
+@i
+D=M
+@24576
+D=A-D
+@LOOP
+D;JLE
+
+@24576
+D=M     // get keyboard pressed button
+@CLEAR
+D;JEQ
+// @-1
+D=-1
+@i
+A=M
+M=D
+@i
+M=M+1
+@BLOOP
+0;JMP
+
+(CLEAR)
+@16384
+D=A
+@i
+M=D
+(CLOOP)
+@i
+D=M
+@24576
+D=A-D
+@LOOP
+D;JLE
+
+@24576
+D=M     // get keyboard pressed button
+@LOOP
+D;JGT
+
+@i
+A=M
+M=0
+@i
+M=M+1
+@CLOOP
+0;JMP
+
+(END)
